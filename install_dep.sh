809d5de7e0cf8279d0def0f4d2aed09c3e5aecf0
Christoffer Henriksson <christoffer301@gmail.com>
2017-12-06 11:03:39 +0100

    added pyaudio to install script

--- install_dep.sh
+++ install_dep.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 echo "Installing general dependencies"
-sudo apt-get install gcc automake autoconf libtool bison swig python-dev libpulse-dev
+sudo apt-get install gcc automake autoconf libtool bison swig python-dev libpulse-dev python-pyaudio
 
 echo "Installing GStreamer dependencies"
 sudo apt-get install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools
